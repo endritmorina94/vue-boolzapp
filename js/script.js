8fe2bad57f9ea6e83557ddd7c515fd6955c9f695
endritmorina94 <endrit.morina@outlook.it>
2021-04-23 17:11:19 +0200

    aggiunto costum scrollbar e migliorato gradiente di sfondo

--- js/script.js
+++ js/script.js
@@ -83,9 +83,81 @@ var app = new Vue({
         			}
         		],
         	},
+            {
+        		name: 'Carlo',
+        		avatar: '_4',
+                isActive: false,
+        		visible: true,
+        		messages: [
+        			{
+        				date: '28/03/2020 10:10:40',
+        				text: 'La Marianna va in campagna',
+        				status: 'received',
+                        dropdown: false
+        			},
+        			{
+        				date: '28/03/2020 10:20:10',
+        				text: 'Sicuro di non aver sbagliato chat?',
+        				status: 'sent',
+                        dropdown: false
+        			},
+        			{
+        				date: '28/03/2020 16:15:22',
+        				text: 'Ah scusa!',
+        				status: 'received',
+                        dropdown: false
+        			}
+        		],
+        	},
+            {
+        		name: 'Vittorio',
+        		avatar: '_5',
+                isActive: false,
+        		visible: true,
+        		messages: [
+        			{
+        				date: '28/03/2020 10:10:40',
+        				text: 'La Marianna va in campagna',
+        				status: 'received',
+                        dropdown: false
+        			},
+        			{
+        				date: '28/03/2020 10:20:10',
+        				text: 'Sicuro di non aver sbagliato chat?',
+        				status: 'sent',
+                        dropdown: false
+        			},
+        			{
+        				date: '28/03/2020 16:15:22',
+        				text: 'Ah scusa!',
+        				status: 'received',
+                        dropdown: false
+        			}
+        		],
+        	},
         	{
         		name: 'Luisa',
         		avatar: '_6',
+                isActive: false,
+        		visible: true,
+        		messages: [
+        			{
+        				date: '10/01/2020 15:30:55',
+        				text: 'Lo sai che ha aperto una nuova pizzeria?',
+        				status: 'sent',
+                        dropdown: false
+        			},
+        			{
+        				date: '10/01/2020 15:50:00',
+        				text: 'Si, ma preferirei andare al cinema',
+        				status: 'received',
+                        dropdown: false
+        			}
+        		],
+        	},
+            {
+        		name: 'Marco',
+        		avatar: '_7',
                 isActive: false,
         		visible: true,
         		messages: [
